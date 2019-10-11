10038fe1ab26c537f708f60c4c8ff323cc964de0
hewenwei038 <54757153+hewenwei038@users.noreply.github.com>
2019-10-11 22:23:13 +0800

    update4

--- assignment4/ja.js
+++ assignment4/ja.js
@@ -1,2 +1,31 @@
- 
+var num = Math.floor(Math.random()*11);
+
+var i=5;
+function checknum(){
+
+var nums = document.getElementById("myForm").elements[0].value;
+
+
+ docount();
+  if(nums-num==0){
+  document.write('<img src="boom.png">');
+}
+else if(nums-num>0){
+    document.getElementById("demo").innerHTML = "You still have " + i +" chances, a little bit small!";
+  }
+else {
+    document.getElementById("demo").innerHTML = "You still have " + i +" chances, a little bit big!";
+  
+  } 
+}
+
+
+function docount(){
+i--;
+if(i==0){
+  document.getElementById("demo").innerHTML = " NOOOO CHANCES!";
+}
+
+
+}
 
