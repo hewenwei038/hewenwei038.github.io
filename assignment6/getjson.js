54a20a6680470aaa061316fcbd82e57b89890191
hewenwei038 <54757153+hewenwei038@users.noreply.github.com>
2019-10-22 19:02:47 +0800

    update assignment6

---
+++ assignment6/getjson.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+
+        var items = [];
+        var i = 0;
+        var airtable_read_endpoint = "https://api.airtable.com/v0/appKIU0zkdHt3AVTL/Product?api_key=keycj6dRwXwYLEjiv";
+        var dataSet = [];
+        $.getJSON(airtable_read_endpoint, function(result) {
+               $.each(result.records, function(key,value) {
+                   items = [];
+                       items.push(value.fields.company);
+                       items.push(value.fields.product_code);
+                       items.push(value.fields.product_no);
+                       items.push(value.fields.geocode2);
+                       items.push(value.fields.json);
+                       items.push(value.fields.gender);
+                       items.push(value.fields.name);
+                       dataSet.push(items);
+                       console.log(items);
+                }); // end .each
+                console.log(dataSet);
+
+             $('#table1').DataTable( {
+                 data: dataSet,
+                 retrieve: true,
+                 columns: [
+                     { title: "Company",
+                       defaultContent:""},
+                     { title: "Product",
+                         defaultContent:"" },
+                     { title: "Number",
+                       defaultContent:"" },
+                     { title: "Geo Code",
+                       defaultContent:""},
+                     { title: "JSON",
+                         defaultContent:""},
+                     { title: "Gender",
+                       defaultContent:""},
+                     { title: "Name",
+                       defaultContent:""},
+                 ]
+             } );
+        }); // end .getJSON
+     }); // end button
+
