28061ff7f9b641cd422215c454982ea9868c0f1c
hewenwei038 <54757153+hewenwei038@users.noreply.github.com>
2019-10-30 20:40:48 +0800

    update

--- assignment7/script.js
+++ assignment7/script.js
@@ -1,5 +1,4 @@
 //chart1
-
 $(document).ready(function(){
     var items = [];
     var i = 0;
@@ -8,15 +7,15 @@ $(document).ready(function(){
     $.getJSON(airtable_read_endpoint, function(result) {
            $.each(result.records, function(key,value) {
                items = [];
-                   items.push(value.Name);
-                   items.push(value.数量);
+                   items.push(value.fields.Name);
+                   items.push(value.fields.数量);
                    dataSet.push(items);
                    console.log(items);
             }); // end .each
             console.log(dataSet);
 
        
-         var chart1 = c3.generate({
+         var chart = c3.generate({
               data: {
                   columns: dataSet,
                   type : 'bar'
@@ -35,3 +34,37 @@ $(document).ready(function(){
  }); // end button
 // document ready
 
+//chart2
+$(document).ready(function(){
+    var items = [];
+    var i = 0;
+    var airtable_read_endpoint = "https://api.airtable.com/v0/appzaLFMIfIdCLiyb/%E8%A1%8C%E6%94%BF%E5%8C%BA?api_key=keyMKBpzuSeqRV0SV";
+    var dataSet = [];
+    $.getJSON(airtable_read_endpoint, function(result) {
+           $.each(result.records, function(key,value) {
+               items = [];
+                   items.push(value.fields.Name);
+                   items.push(value.fields.数量);
+                   dataSet.push(items);
+                   console.log(items);
+            }); // end .each
+            console.log(dataSet);
+
+       
+         var chart= c3.generate({
+              data: {
+                  columns: dataSet,
+                  type : 'bar'
+              },
+              axis: {
+                x: {label: 'Product'},
+                y: {label: '# of Items'}
+              },
+              bar: {
+                  title: "# of Items by Product Category:",
+              }
+          });
+
+    }); // end .getJSON
+
+ });
